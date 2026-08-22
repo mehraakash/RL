684c0f9d959d1b7246ed196983cde953de43e6bb
Akash Mehra <akamehra@nvidia.com>
2026-08-22 07:00:11 +0000

    fix(sc): isolate NeMo Gym rollout attempts
    Signed-off-by: Akash Mehra <akamehra@nvidia.com>

--- nemo_rl/experience/interfaces.py
+++ nemo_rl/experience/interfaces.py
@@ -18,6 +18,7 @@ from typing import Any, Optional
 from nemo_rl.data.interfaces import LLMMessageLogType, VLMMessageLogType
 
 NEMO_GYM_TASK_INDEX_KEY = "_ng_task_index"
+NEMO_GYM_ROLLOUT_INDEX_KEY = "_ng_rollout_index"
 NEXT_NEMO_GYM_TASK_INDEX_KEY = "next_ng_task_index"
 
 
